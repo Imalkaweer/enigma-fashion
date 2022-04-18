af488e11ea6c0f50ecd735dc2bb41646a42fc9e8
Imalka Weerasekara <imajavaweerasekara128@gmail.com>
2022-04-18 14:02:32 +0530

    optimized code and adding css

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../asserts/crown.svg";
+import "./navigation.style.scss"
 
 const NavigationBar = () => {
   return (
     <Fragment>
       <div className="navigation">
-        <Link className="logo" to={"/"}>
-          LOGO
+        <Link className="logo-container" to={"/"}>
+          <CrownLogo className="logo"/>
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to={"/shop"}>
