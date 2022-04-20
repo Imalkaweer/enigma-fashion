9dff528a8e92a4fbac95a132163cacc5fd6ba3a3
Imalka Weerasekara <imajavaweerasekara128@gmail.com>
2022-04-20 22:48:08 +0530

    added firebase dependency and added sign-in component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import NavigationBar from "./routes/navigation/navigation.component";
+import SignIn from "./components/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>Hi I am Enigma Shop!</h1>;
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<NavigationBar />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
